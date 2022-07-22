a51262d125eecc11af0914358be167488527b27f
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-07-22 15:25:08 -0400

    Installing MUI for testing

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './Home.css';
 import Header from './Header';
 import NavBar from './NavBar'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function Home() {
   return (
